d51fc9660b6eef50e650d1f2d669309fa2e37ab2
Estefanía <estefania.gorosito.16@gmail.com>
2021-02-09 18:59:15 -0300

    Agrego forms para crear, modificar y listar clientes. Sin funcionalidad aún

---
+++ EscritorioGestionProyectosLiquidaciones/Clientes/crearModificarClientesForm.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace EscritorioGestionProyectosLiquidaciones.Clientes
+{
+    partial class crearModificarClientesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.nombreClienteTxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dropdownLocalidades = new System.Windows.Forms.ComboBox();
+            this.mailTxt = new System.Windows.Forms.TextBox();
+            this.apellidoClienteTxt = new System.Windows.Forms.TextBox();
+            this.direccionTxt = new System.Windows.Forms.TextBox();
+            this.telefonoTxt = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dropdownProvincias = new System.Windows.Forms.ComboBox();
+            this.volverBtn = new System.Windows.Forms.Button();
+            this.guardarBtn = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Crear cliente";
+            // 
+            // nombreClienteTxt
+            // 
+            this.nombreClienteTxt.Location = new System.Drawing.Point(37, 91);
+            this.nombreClienteTxt.Name = "nombreClienteTxt";
+            this.nombreClienteTxt.Size = new System.Drawing.Size(166, 20);
+            this.nombreClienteTxt.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(34, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(216, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Apellido";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(403, 71);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Dirección";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(34, 139);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Teléfono";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(312, 139);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Email";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.dropdownLocalidades);
+            this.panel1.Controls.Add(this.mailTxt);
+            this.panel1.Controls.Add(this.apellidoClienteTxt);
+            this.panel1.Controls.Add(this.direccionTxt);
+            this.panel1.Controls.Add(this.telefonoTxt);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.dropdownProvincias);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.volverBtn);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.guardarBtn);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.nombreClienteTxt);
+            this.panel1.Location = new System.Drawing.Point(77, 54);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(614, 338);
+            this.panel1.TabIndex = 11;
+            // 
+            // dropdownLocalidades
+            // 
+            this.dropdownLocalidades.FormattingEnabled = true;
+            this.dropdownLocalidades.Location = new System.Drawing.Point(315, 230);
+            this.dropdownLocalidades.Name = "dropdownLocalidades";
+            this.dropdownLocalidades.Size = new System.Drawing.Size(252, 21);
+            this.dropdownLocalidades.TabIndex = 18;
+            // 
+            // mailTxt
+            // 
+            this.mailTxt.Location = new System.Drawing.Point(315, 159);
+            this.mailTxt.Name = "mailTxt";
+            this.mailTxt.Size = new System.Drawing.Size(257, 20);
+            this.mailTxt.TabIndex = 17;
+            // 
+            // apellidoClienteTxt
+            // 
+            this.apellidoClienteTxt.Location = new System.Drawing.Point(219, 91);
+            this.apellidoClienteTxt.Name = "apellidoClienteTxt";
+            this.apellidoClienteTxt.Size = new System.Drawing.Size(166, 20);
+            this.apellidoClienteTxt.TabIndex = 16;
+            // 
+            // direccionTxt
+            // 
+            this.direccionTxt.Location = new System.Drawing.Point(406, 91);
+            this.direccionTxt.Name = "direccionTxt";
+            this.direccionTxt.Size = new System.Drawing.Size(166, 20);
+            this.direccionTxt.TabIndex = 15;
+            // 
+            // telefonoTxt
+            // 
+            this.telefonoTxt.Location = new System.Drawing.Point(37, 159);
+            this.telefonoTxt.Name = "telefonoTxt";
+            this.telefonoTxt.Size = new System.Drawing.Size(252, 20);
+            this.telefonoTxt.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(312, 210);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(69, 17);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Localidad";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(34, 210);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(66, 17);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Provincia";
+            // 
+            // dropdownProvincias
+            // 
+            this.dropdownProvincias.FormattingEnabled = true;
+            this.dropdownProvincias.Location = new System.Drawing.Point(37, 230);
+            this.dropdownProvincias.Name = "dropdownProvincias";
+            this.dropdownProvincias.Size = new System.Drawing.Size(252, 21);
+            this.dropdownProvincias.TabIndex = 2;
+            // 
+            // volverBtn
+            // 
+            this.volverBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.volverBtn.Location = new System.Drawing.Point(37, 281);
+            this.volverBtn.Name = "volverBtn";
+            this.volverBtn.Size = new System.Drawing.Size(125, 30);
+            this.volverBtn.TabIndex = 1;
+            this.volverBtn.Text = "Volver";
+            this.volverBtn.UseVisualStyleBackColor = true;
+            // 
+            // guardarBtn
+            // 
+            this.guardarBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guardarBtn.Location = new System.Drawing.Point(168, 281);
+            this.guardarBtn.Name = "guardarBtn";
+            this.guardarBtn.Size = new System.Drawing.Size(125, 30);
+            this.guardarBtn.TabIndex = 0;
+            this.guardarBtn.Text = "Guardar cliente";
+            this.guardarBtn.UseVisualStyleBackColor = true;
+            // 
+            // crearClientesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "crearClientesForm";
+            this.Text = "Crear cliente";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nombreClienteTxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox dropdownLocalidades;
+        private System.Windows.Forms.TextBox mailTxt;
+        private System.Windows.Forms.TextBox apellidoClienteTxt;
+        private System.Windows.Forms.TextBox direccionTxt;
+        private System.Windows.Forms.TextBox telefonoTxt;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox dropdownProvincias;
+        private System.Windows.Forms.Button volverBtn;
+        private System.Windows.Forms.Button guardarBtn;
+    }
+}
---
+++ EscritorioGestionProyectosLiquidaciones/Clientes/gestionarClientesForm.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace EscritorioGestionProyectosLiquidaciones.Clientes
+{
+    partial class gestionarClientesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.volverBtn = new System.Windows.Forms.Button();
+            this.crearClienteBtn = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.crearClienteBtn);
+            this.panel1.Controls.Add(this.volverBtn);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Location = new System.Drawing.Point(51, 43);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(688, 354);
+            this.panel1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(21, 70);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(647, 225);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(18, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Lista de clientes";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(18, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Cree y modifique clientes";
+            // 
+            // volverBtn
+            // 
+            this.volverBtn.Location = new System.Drawing.Point(22, 310);
+            this.volverBtn.Name = "volverBtn";
+            this.volverBtn.Size = new System.Drawing.Size(75, 23);
+            this.volverBtn.TabIndex = 3;
+            this.volverBtn.Text = "Volver";
+            this.volverBtn.UseVisualStyleBackColor = true;
+            // 
+            // crearClienteBtn
+            // 
+            this.crearClienteBtn.Location = new System.Drawing.Point(103, 310);
+            this.crearClienteBtn.Name = "crearClienteBtn";
+            this.crearClienteBtn.Size = new System.Drawing.Size(122, 23);
+            this.crearClienteBtn.TabIndex = 4;
+            this.crearClienteBtn.Text = "Crear nuevo cliente";
+            this.crearClienteBtn.UseVisualStyleBackColor = true;
+            // 
+            // gestionarClientesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "gestionarClientesForm";
+            this.Text = "Gestionar clientes";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button crearClienteBtn;
+        private System.Windows.Forms.Button volverBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
