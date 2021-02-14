d18ee49b7f3d642bc309a73cf71eefcb39601d5b
Estefanía <estefania.gorosito.16@gmail.com>
2021-02-13 22:16:02 -0300

    Agrego modelo

---
+++ EscritorioGestionProyectosLiquidaciones/Models/EmpleadoProyecto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EscritorioGestionProyectosLiquidaciones.Models
+{
+    public partial class EmpleadoProyecto
+    {
+        public int Idempleado { get; set; }
+        public int Idproyecto { get; set; }
+
+        public Empleado IdempleadoNavigation { get; set; }
+        public Proyecto IdproyectoNavigation { get; set; }
+    }
+}
---
+++ EscritorioGestionProyectosLiquidaciones/Models/Provincia.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EscritorioGestionProyectosLiquidaciones.Models
+{
+    public partial class Provincia
+    {
+        public Provincia()
+        {
+            Localidad = new HashSet<Localidad>();
+        }
+
+        public long Idprovincia { get; set; }
+        public string Descripcion { get; set; }
+
+        public ICollection<Localidad> Localidad { get; set; }
+    }
+}
