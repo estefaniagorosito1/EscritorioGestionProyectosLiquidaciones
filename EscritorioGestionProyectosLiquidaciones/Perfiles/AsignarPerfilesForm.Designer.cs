ea6582f53e96e09290308d9bb47c650cc400eb13
Estefanía <estefania.gorosito.16@gmail.com>
2021-02-16 20:06:08 -0300

    Agrego pantalla para asignar perfiles a un empleado

---
+++ EscritorioGestionProyectosLiquidaciones/Perfiles/AsignarPerfilesForm.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace EscritorioGestionProyectosLiquidaciones
+{
+    partial class AsignarPerfilesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.guardarBtn = new System.Windows.Forms.Button();
+            this.volverBtn = new System.Windows.Forms.Button();
+            this.supervisorCheck = new System.Windows.Forms.CheckBox();
+            this.capacitadorCheck = new System.Windows.Forms.CheckBox();
+            this.implementadorCheck = new System.Windows.Forms.CheckBox();
+            this.testerCheck = new System.Windows.Forms.CheckBox();
+            this.desarrolladorCheck = new System.Windows.Forms.CheckBox();
+            this.analistaCheck = new System.Windows.Forms.CheckBox();
+            this.nombreEmpleadoTxt = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.guardarBtn);
+            this.panel1.Controls.Add(this.volverBtn);
+            this.panel1.Controls.Add(this.supervisorCheck);
+            this.panel1.Controls.Add(this.capacitadorCheck);
+            this.panel1.Controls.Add(this.implementadorCheck);
+            this.panel1.Controls.Add(this.testerCheck);
+            this.panel1.Controls.Add(this.desarrolladorCheck);
+            this.panel1.Controls.Add(this.analistaCheck);
+            this.panel1.Controls.Add(this.nombreEmpleadoTxt);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(35, 29);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 346);
+            this.panel1.TabIndex = 0;
+            // 
+            // guardarBtn
+            // 
+            this.guardarBtn.Location = new System.Drawing.Point(192, 279);
+            this.guardarBtn.Name = "guardarBtn";
+            this.guardarBtn.Size = new System.Drawing.Size(75, 23);
+            this.guardarBtn.TabIndex = 9;
+            this.guardarBtn.Text = "Guardar";
+            this.guardarBtn.UseVisualStyleBackColor = true;
+            this.guardarBtn.Click += new System.EventHandler(this.guardarBtn_Click);
+            // 
+            // volverBtn
+            // 
+            this.volverBtn.Location = new System.Drawing.Point(95, 279);
+            this.volverBtn.Name = "volverBtn";
+            this.volverBtn.Size = new System.Drawing.Size(75, 23);
+            this.volverBtn.TabIndex = 8;
+            this.volverBtn.Text = "Volver";
+            this.volverBtn.UseVisualStyleBackColor = true;
+            this.volverBtn.Click += new System.EventHandler(this.volverBtn_Click);
+            // 
+            // supervisorCheck
+            // 
+            this.supervisorCheck.AutoSize = true;
+            this.supervisorCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.supervisorCheck.Location = new System.Drawing.Point(47, 230);
+            this.supervisorCheck.Name = "supervisorCheck";
+            this.supervisorCheck.Size = new System.Drawing.Size(114, 19);
+            this.supervisorCheck.TabIndex = 7;
+            this.supervisorCheck.Text = "Supervisor (PM)";
+            this.supervisorCheck.UseVisualStyleBackColor = true;
+            // 
+            // capacitadorCheck
+            // 
+            this.capacitadorCheck.AutoSize = true;
+            this.capacitadorCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.capacitadorCheck.Location = new System.Drawing.Point(47, 200);
+            this.capacitadorCheck.Name = "capacitadorCheck";
+            this.capacitadorCheck.Size = new System.Drawing.Size(92, 19);
+            this.capacitadorCheck.TabIndex = 6;
+            this.capacitadorCheck.Text = "Capacitador";
+            this.capacitadorCheck.UseVisualStyleBackColor = true;
+            // 
+            // implementadorCheck
+            // 
+            this.implementadorCheck.AutoSize = true;
+            this.implementadorCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.implementadorCheck.Location = new System.Drawing.Point(47, 166);
+            this.implementadorCheck.Name = "implementadorCheck";
+            this.implementadorCheck.Size = new System.Drawing.Size(110, 19);
+            this.implementadorCheck.TabIndex = 5;
+            this.implementadorCheck.Text = "Implementador";
+            this.implementadorCheck.UseVisualStyleBackColor = true;
+            // 
+            // testerCheck
+            // 
+            this.testerCheck.AutoSize = true;
+            this.testerCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.testerCheck.Location = new System.Drawing.Point(47, 132);
+            this.testerCheck.Name = "testerCheck";
+            this.testerCheck.Size = new System.Drawing.Size(60, 19);
+            this.testerCheck.TabIndex = 4;
+            this.testerCheck.Text = "Tester";
+            this.testerCheck.UseVisualStyleBackColor = true;
+            // 
+            // desarrolladorCheck
+            // 
+            this.desarrolladorCheck.AutoSize = true;
+            this.desarrolladorCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.desarrolladorCheck.Location = new System.Drawing.Point(47, 100);
+            this.desarrolladorCheck.Name = "desarrolladorCheck";
+            this.desarrolladorCheck.Size = new System.Drawing.Size(101, 19);
+            this.desarrolladorCheck.TabIndex = 3;
+            this.desarrolladorCheck.Text = "Desarrollador";
+            this.desarrolladorCheck.UseVisualStyleBackColor = true;
+            // 
+            // analistaCheck
+            // 
+            this.analistaCheck.AutoSize = true;
+            this.analistaCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.analistaCheck.Location = new System.Drawing.Point(47, 68);
+            this.analistaCheck.Name = "analistaCheck";
+            this.analistaCheck.Size = new System.Drawing.Size(69, 19);
+            this.analistaCheck.TabIndex = 2;
+            this.analistaCheck.Text = "Analista";
+            this.analistaCheck.UseVisualStyleBackColor = true;
+            // 
+            // nombreEmpleadoTxt
+            // 
+            this.nombreEmpleadoTxt.AutoSize = true;
+            this.nombreEmpleadoTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nombreEmpleadoTxt.Location = new System.Drawing.Point(208, 23);
+            this.nombreEmpleadoTxt.Name = "nombreEmpleadoTxt";
+            this.nombreEmpleadoTxt.Size = new System.Drawing.Size(0, 17);
+            this.nombreEmpleadoTxt.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Asignar perfiles al empleado:";
+            // 
+            // AsignarPerfilesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(476, 411);
+            this.Controls.Add(this.panel1);
+            this.Name = "AsignarPerfilesForm";
+            this.Text = "Perfiles";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button guardarBtn;
+        private System.Windows.Forms.Button volverBtn;
+        private System.Windows.Forms.CheckBox supervisorCheck;
+        private System.Windows.Forms.CheckBox capacitadorCheck;
+        private System.Windows.Forms.CheckBox implementadorCheck;
+        private System.Windows.Forms.CheckBox testerCheck;
+        private System.Windows.Forms.CheckBox desarrolladorCheck;
+        private System.Windows.Forms.CheckBox analistaCheck;
+        private System.Windows.Forms.Label nombreEmpleadoTxt;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ EscritorioGestionProyectosLiquidaciones/Perfiles/AsignarPerfilesForm.cs
@@ -0,0 +1,139 @@
+﻿using EscritorioGestionProyectosLiquidaciones.Models;
+using EscritorioGestionProyectosLiquidaciones.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EscritorioGestionProyectosLiquidaciones
+{
+    public partial class AsignarPerfilesForm : Form
+    {
+        private PerfilService _perfilService;
+        private PerfilEmpleadoService _perfilEmpleadoService;
+        private int IdEmpleado;
+
+        public AsignarPerfilesForm()
+        {
+            InitializeComponent();
+            _perfilService = new PerfilService();
+            _perfilEmpleadoService = new PerfilEmpleadoService();
+        }
+
+        public void SetIdEmpleado(int IdEmpleado)
+        {
+            this.IdEmpleado = IdEmpleado;
+        }
+
+        public void LoadNombreEmpleado(string nombre)
+        {
+            nombreEmpleadoTxt.Text = nombre;
+        }
+
+        public void LoadPerfiles(List<Perfil> perfiles)
+        {
+            foreach (var item in perfiles)
+            {
+                if (item.NombrePerfil.Equals("Analista"))
+                {
+                    analistaCheck.Checked = true;
+                }
+
+                if (item.NombrePerfil.Equals("Desarrollador"))
+                {
+                    desarrolladorCheck.Checked = true;
+                }
+
+                if (item.NombrePerfil.Equals("Tester"))
+                {
+                    testerCheck.Checked = true;
+                }
+
+                if (item.NombrePerfil.Equals("Implementador"))
+                {
+                    implementadorCheck.Checked = true;
+                }
+
+                if (item.NombrePerfil.Equals("Capacitador"))
+                {
+                    capacitadorCheck.Checked = true;
+                }
+
+                if (item.NombrePerfil.Equals("Supervisor"))
+                {
+                    supervisorCheck.Checked = true;
+                }
+            }
+        }
+
+        private void volverBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void guardarBtn_Click(object sender, EventArgs e)
+        {
+            List<Perfil> perfiles = _perfilService.FindPerfiles();
+            List<Perfil> perfilesAAsignar = new List<Perfil>();
+            List<PerfilEmpleado> perfilesEmpleado = new List<PerfilEmpleado>();
+            Perfil perfil = null;
+
+            if (analistaCheck.Checked)
+            {
+                perfil = perfiles.Find(p => p.NombrePerfil == "Analista");
+                perfilesAAsignar.Add(perfil);
+            }
+
+            if (desarrolladorCheck.Checked)
+            {
+                perfil = perfiles.Find(p => p.NombrePerfil == "Desarrollador");
+                perfilesAAsignar.Add(perfil);
+            }
+
+            if (testerCheck.Checked)
+            {
+                perfil = perfiles.Find(p => p.NombrePerfil == "Tester");
+                perfilesAAsignar.Add(perfil);
+            }
+
+            if (implementadorCheck.Checked)
+            {
+                perfil = perfiles.Find(p => p.NombrePerfil == "Implementador");
+                perfilesAAsignar.Add(perfil);
+            }
+
+            if (capacitadorCheck.Checked)
+            {
+                perfil = perfiles.Find(p => p.NombrePerfil == "Capacitador");
+                perfilesAAsignar.Add(perfil);
+            }
+
+            if (supervisorCheck.Checked)
+            {
+                perfil = perfiles.Find(p => p.NombrePerfil == "Supervisor");
+                perfilesAAsignar.Add(perfil);
+            }
+
+            foreach (var item in perfilesAAsignar)
+            {
+                Console.WriteLine(item.ToString());
+
+                PerfilEmpleado perfilEmpleado = new PerfilEmpleado();
+                perfilEmpleado.Idperfil = item.Idperfil;
+                perfilEmpleado.Idempleado = IdEmpleado;
+
+                Console.WriteLine(perfilEmpleado.Idperfil.ToString());
+                perfilesEmpleado.Add(perfilEmpleado);
+            }
+
+
+            // _perfilEmpleadoService.AsignarPerfilesEmpleado(perfilesEmpleado);
+
+        }
+    }
+}
