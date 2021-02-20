1dfae2d8b4933464e0c4e095b3eae909ddc83a62
Estefanía <estefania.gorosito.16@gmail.com>
2021-02-20 17:41:38 -0300

    Agrego form para cargar horas a una tarea

---
+++ EscritorioGestionProyectosLiquidaciones/Tareas/CargarHorasTarea.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace EscritorioGestionProyectosLiquidaciones.Tareas
+{
+    partial class CargarHorasTarea
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.horasTrabajadasTxt = new System.Windows.Forms.TextBox();
+            this.horasEstimadasTxt = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.guardarBtn = new System.Windows.Forms.Button();
+            this.volverBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.horasACargar = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.horasACargar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.horasTrabajadasTxt);
+            this.panel1.Controls.Add(this.horasEstimadasTxt);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.guardarBtn);
+            this.panel1.Controls.Add(this.volverBtn);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.horasACargar);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(338, 252);
+            this.panel1.TabIndex = 0;
+            // 
+            // horasTrabajadasTxt
+            // 
+            this.horasTrabajadasTxt.Location = new System.Drawing.Point(157, 154);
+            this.horasTrabajadasTxt.Name = "horasTrabajadasTxt";
+            this.horasTrabajadasTxt.Size = new System.Drawing.Size(165, 20);
+            this.horasTrabajadasTxt.TabIndex = 9;
+            // 
+            // horasEstimadasTxt
+            // 
+            this.horasEstimadasTxt.Location = new System.Drawing.Point(157, 109);
+            this.horasEstimadasTxt.Name = "horasEstimadasTxt";
+            this.horasEstimadasTxt.Size = new System.Drawing.Size(165, 20);
+            this.horasEstimadasTxt.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(22, 65);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 18);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Horas a cargar";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(21, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 22);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Cargar horas";
+            // 
+            // guardarBtn
+            // 
+            this.guardarBtn.Location = new System.Drawing.Point(106, 211);
+            this.guardarBtn.Name = "guardarBtn";
+            this.guardarBtn.Size = new System.Drawing.Size(75, 23);
+            this.guardarBtn.TabIndex = 5;
+            this.guardarBtn.Text = "Guardar";
+            this.guardarBtn.UseVisualStyleBackColor = true;
+            this.guardarBtn.Click += new System.EventHandler(this.guardarBtn_Click);
+            // 
+            // volverBtn
+            // 
+            this.volverBtn.Location = new System.Drawing.Point(25, 211);
+            this.volverBtn.Name = "volverBtn";
+            this.volverBtn.Size = new System.Drawing.Size(75, 23);
+            this.volverBtn.TabIndex = 4;
+            this.volverBtn.Text = "Volver";
+            this.volverBtn.UseVisualStyleBackColor = true;
+            this.volverBtn.Click += new System.EventHandler(this.volverBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Horas trabajadas";
+            // 
+            // horasACargar
+            // 
+            this.horasACargar.Location = new System.Drawing.Point(157, 65);
+            this.horasACargar.Name = "horasACargar";
+            this.horasACargar.Size = new System.Drawing.Size(165, 20);
+            this.horasACargar.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Horas estimadas";
+            // 
+            // CargarHorasTarea
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(363, 287);
+            this.Controls.Add(this.panel1);
+            this.Name = "CargarHorasTarea";
+            this.Text = "Cargar horas";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.horasACargar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox horasTrabajadasTxt;
+        private System.Windows.Forms.TextBox horasEstimadasTxt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button guardarBtn;
+        private System.Windows.Forms.Button volverBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown horasACargar;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ EscritorioGestionProyectosLiquidaciones/Tareas/CargarHorasTarea.cs
@@ -0,0 +1,91 @@
+﻿using EscritorioGestionProyectosLiquidaciones.Models;
+using EscritorioGestionProyectosLiquidaciones.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EscritorioGestionProyectosLiquidaciones.Tareas
+{
+    public partial class CargarHorasTarea : Form
+    {
+        private TareaService _tareaService;
+        private int _idTarea;
+        private int _idEmpleado;
+
+        public CargarHorasTarea()
+        {
+            InitializeComponent();
+            _tareaService = new TareaService();
+            _idTarea = 0;
+            _idEmpleado = 0;
+        }
+
+        public void LoadHorasTarea(int horasEstimadas, int horasTrabajadas, int idTarea, int idEmpleado)
+        {
+            _idTarea = idTarea;
+            _idEmpleado = idEmpleado;
+
+            horasEstimadasTxt.Text = horasEstimadas.ToString();
+            horasEstimadasTxt.Enabled = false;
+            horasTrabajadasTxt.Text = horasTrabajadas.ToString();
+            horasTrabajadasTxt.Enabled = false;
+        }
+
+        private void volverBtn_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void guardarBtn_Click(object sender, EventArgs e)
+        {
+            var horas = horasACargar.Value;
+            var horasTrabajadas = int.Parse(horasTrabajadasTxt.Text.ToString());
+            var horasEstimadas = int.Parse(horasEstimadasTxt.Text.ToString());
+
+            Tarea tarea = _tareaService.FindTarea(_idTarea);
+
+            if (tarea.HorasOverbudget == null)
+            {
+                tarea.HorasOverbudget = 0;
+            }
+
+            if (horas + horasTrabajadas > horasEstimadas)
+            {
+                var horasOverbudget = ((horas + horasTrabajadas) - horasEstimadas) - tarea.HorasOverbudget;
+
+                DialogResult result = MessageBox.Show("Está cargando más horas de las estimadas. ¿Esto es correcto?", "Atencion", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+
+                switch (result)
+                {
+                    case DialogResult.Yes:
+                        tarea.HorasOverbudget = tarea.HorasOverbudget + (int?)horasOverbudget;
+                        CargarHoras(tarea, (int)horas);
+                        break;
+                    case DialogResult.No:
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                CargarHoras(tarea, (int)horas);
+            }
+        }
+
+        private void CargarHoras(Tarea tarea, int horas)
+        {
+            tarea.HorasTrabajadas = tarea.HorasTrabajadas + horas;
+            _tareaService.Guardar(tarea);
+            MessageBox.Show("Horas cargadas correctamente");
+            ((ListaTareasEmpleadoForm)this.Owner).LoadTareasEmpleado(_idEmpleado);
+            Close();
+        }
+    }
+}
